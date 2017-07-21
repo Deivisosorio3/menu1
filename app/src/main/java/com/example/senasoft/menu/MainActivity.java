c2966b56fdf8c43130b0254a7f333eb7393444ac
oscarvc <ing.odvega@gmail.com>
2017-07-21 18:37:32 -0500

    mostrar drawer

--- app/src/main/java/com/example/senasoft/menu/MainActivity.java
+++ app/src/main/java/com/example/senasoft/menu/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.senasoft.menu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (R.id.opt_buscar == item.getItemId()){
+
+            Intent i = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(i);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
